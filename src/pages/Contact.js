ed512c5443b7b1fca504895efa1d2834dd2a52a0
Mohamed Tharwat <74350716+MohamedTharwat940@users.noreply.github.com>
2024-09-19 20:06:53 +0300

    Add files via upload

---
+++ src/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PageHeading from "../common/PageHeading";
+
+const ContactUs = () => {
+  return (
+    <div>
+      <PageHeading home={"home"} pagename={"Contact Us"} />
+      <div className="w-10/12 m-auto">
+        <div className="my-8 text-3xl font-bold">Contact Us Num:- 01205207376</div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
